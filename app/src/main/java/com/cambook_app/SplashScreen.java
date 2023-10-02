d4cdfe5023c653f51f8e1b027c74c2990a28a9fc
KENNETH <kennethmalafo51@gmail.com>
2023-10-02 13:26:14 +0800

    Log_In Commit

--- app/src/main/java/com/cambook_app/SplashScreen.java
+++ app/src/main/java/com/cambook_app/SplashScreen.java
@@ -2,13 +2,45 @@ package com.cambook_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN = 2500;
+    ImageView logo1, logo2;
+    TextView apptxt;
+    Animation top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        logo1 = findViewById(R.id.logopic);
+        logo2 = findViewById(R.id.logotxt);
+        apptxt = findViewById(R.id.app_txt);
+
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
+        logo1.setAnimation(top);
+        logo2.setAnimation(bottom);
+        apptxt.setAnimation(bottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
